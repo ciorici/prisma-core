63b8593f51678cba3b25e39b19b594b50d182b8a
Pavel Ciorici <cioricip@gmail.com>
2026-02-19 10:49:53 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -122,6 +122,19 @@ final class Prisma_Core {
 		if ( ! defined( 'PRISMA_CORE_THEME_PATH' ) ) {
 			define( 'PRISMA_CORE_THEME_PATH', get_parent_theme_file_path() );
 		}
+
+		// Backward-compatible aliases for sinatra-core plugin.
+		if ( ! defined( 'SINATRA_THEME_VERSION' ) ) {
+			define( 'SINATRA_THEME_VERSION', $this->version );
+		}
+
+		if ( ! defined( 'SINATRA_THEME_URI' ) ) {
+			define( 'SINATRA_THEME_URI', PRISMA_CORE_THEME_URI );
+		}
+
+		if ( ! defined( 'SINATRA_THEME_PATH' ) ) {
+			define( 'SINATRA_THEME_PATH', PRISMA_CORE_THEME_PATH );
+		}
 	}
 
 	/**
